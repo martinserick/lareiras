91740f7594b23e2a5ca9b34e7f08ab0f8b5bd4c3
Erick Martins <dkzera10@gmail.com>
2022-05-09 23:01:03 -0300

    Iniciando Projeto

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Icones Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- Link CSS do Projeto -->
+    <link rel="stylesheet" href="/css/style.css">
+    <title>.: Lareiras Ecológicas :.</title>
+</head>
+
+<body>
+    <!-- Cabeçalho do Site -->
+    <header>
+        <div id="log-site">
+            <img src="img/logo.png" alt="" srcset="">
+        </div>
+        <div id="navbar">
+            <ul>
+                <li>
+                    <a href="#quem_somos">Quem Somos</a>
+                </li>
+                <li>
+                    <a href="#produtos_site">Produtos</a>
+                </li>
+                <!-- <li>
+                    <a href="#destaques">Destaques</a>
+                </li> -->
+                <li>
+                    <a href="#contato">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- Conteudo Principal -->
+    <main>
+        <section id="quem_somos">
+            <h2>Quem Somos</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, sint quos ut reiciendis unde sunt vero
+                placeat. Doloremque dolore autem ex dignissimos illo aliquid reiciendis cum dolorum est assumenda! Id
+                quas asperiores ad quibusdam dolorem odit aperiam deserunt ipsam eligendi commodi veniam in facere est
+                laboriosam et assumenda omnis harum quaerat molestiae illum, sint temporibus? Consectetur similique
+                voluptatibus sed saepe iste nostrum suscipit natus quo, inventore nihil esse culpa ipsam impedit
+                provident dolores ad possimus enim, quae deleniti vel. Incidunt tempore magni sint aut iure! Quam facere
+                unde ullam! Dolore sequi et quam maxime labore ad, adipisci, nam doloremque officiis magni rem.
+                Molestias aut dolorum deserunt quibusdam voluptatum perferendis, impedit error id expedita neque. Nobis
+                optio voluptas itaque repellat pariatur. Architecto, iusto exercitationem possimus esse dignissimos
+                expedita ipsa sapiente dicta voluptatem fuga praesentium, harum numquam aliquid, quidem sit in culpa!
+                Odio velit labore illum fuga cumque tempore ipsam sapiente animi deleniti possimus! Quasi, ipsa. Quas
+                nisi, ut magni, voluptatem quae aliquid repellendus praesentium iusto totam temporibus explicabo commodi
+                et perspiciatis quo possimus laborum inventore earum odit placeat cum facilis? Fuga, mollitia saepe at
+                facere dolorum vero perferendis quasi dolore ut ipsum aperiam. Aut amet ipsa libero saepe unde eos
+                doloribus.</p>
+        </section>
+        <section id="produtos_site">
+            <h2>Produtos</h2>
+            <div id="produtos">
+                <div class="produto">
+                    <img src="img/lareira_simples.jpg" alt="Lareira Simples">
+                    <h3>Lareira Simples</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero a eum repudiandae nisi praesentium
+                        sequi saepe minima molestias ea tempore ut nulla voluptate quod quisquam obcaecati vel, modi
+                        laboriosam aliquid?</p>
+                    <h5>R$ 70,00</h5>
+                </div>
+                <div class="produto">
+                    <img src="img/lareira_dupla.jpg" alt="Lareira Dupla">
+                    <h3>Lareira Dupla</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero a eum repudiandae nisi praesentium
+                        sequi saepe minima molestias ea tempore ut nulla voluptate quod quisquam obcaecati vel, modi
+                        laboriosam aliquid?</p>
+                    <h5>R$ 70,00</h5>
+                </div>
+                <div class="produto">
+                    <img src="img/lareira_funcionando.jpg" alt="Lareira Dupla">
+                    <h3>Nome do Produto</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero a eum repudiandae nisi praesentium
+                        sequi saepe minima molestias ea tempore ut nulla voluptate quod quisquam obcaecati vel, modi
+                        laboriosam aliquid?</p>
+                    <h5>R$ 70,00</h5>
+                </div>
+            </div>
+        </section>
+        <section id="contato">
+            <h2>Contato</h2>
+            <div id="formulario">
+                <div>
+                    <h3>Entre em contato conosco..</h3>
+                    <p>Preencha o formulário, entraremos em contato o mais breve possível!</p>
+                </div>
+                <form action="">
+                    <input type="text" placeholder="Nome">
+                    <input type="email" placeholder="Seu melhor e-mail">
+                    <input type="tel" placeholder="Telefone" data-mask="(00)00000-0000">
+                    <textarea name="solicitacao" id="pedido" cols="30" rows="10" placeholder="Digite sua dúvida ou solicitação..."></textarea>
+                    <input type="submit" value="Enviar" id="btn--envio">
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <!-- Rodapé do Site -->
+
+    <footer>
+        <div id="rodape">
+            <div id="dados_empresa">
+                <p><span>Endereço:</span> Rua Alda Bassete Bertholdi, 2171</p>
+                <p>CNPJ: 80.359.789/0001-33</p>
+                <p>Telefone: 41 997532094</p>
+                <p>E-mail: contato@madeiracomarte.com.br</p>
+            </div>
+            <div id="cotacao">
+                <h2>Solicite um orçamento!</h2>
+                <a href="/contato.html">
+                    <button>Orçamento</button>
+                </a>
+            </div>
+            <div id="redes_sociais">
+                <p>Siga-nos</p>
+                <ul>
+                    <li>
+                        <a id="instagram" href="#"><i class="bi bi-instagram"></i></a>
+                    </li>
+                    <li>
+                        <a id="facebook" href="#"><i class="bi bi-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a id="youtube" href="#"><i class="bi bi-youtube"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div id="copy">
+            <p>&copy; Copyright Arte Madeira | Todos os direitos reservados</p>
+        </div>
+    </footer>
+
+    <!-- Botão Whatsapp -->
+    <a href="https://api.whatsapp.com/send?phone=5541997532094&text=Olá, tenho interesse nos seus produtos!"
+    target="_blank"
+    style="position:fixed;bottom:20px;right:30px;">
+    <svg enable-background="new 0 0 512 512" width="50" height="50" version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><path d="M256.064,0h-0.128l0,0C114.784,0,0,114.816,0,256c0,56,18.048,107.904,48.736,150.048l-31.904,95.104  l98.4-31.456C155.712,496.512,204,512,256.064,512C397.216,512,512,397.152,512,256S397.216,0,256.064,0z" fill="#4CAF50"/><path d="m405.02 361.5c-6.176 17.44-30.688 31.904-50.24 36.128-13.376 2.848-30.848 5.12-89.664-19.264-75.232-31.168-123.68-107.62-127.46-112.58-3.616-4.96-30.4-40.48-30.4-77.216s18.656-54.624 26.176-62.304c6.176-6.304 16.384-9.184 26.176-9.184 3.168 0 6.016 0.16 8.576 0.288 7.52 0.32 11.296 0.768 16.256 12.64 6.176 14.88 21.216 51.616 23.008 55.392 1.824 3.776 3.648 8.896 1.088 13.856-2.4 5.12-4.512 7.392-8.288 11.744s-7.36 7.68-11.136 12.352c-3.456 4.064-7.36 8.416-3.008 15.936 4.352 7.36 19.392 31.904 41.536 51.616 28.576 25.44 51.744 33.568 60.032 37.024 6.176 2.56 13.536 1.952 18.048-2.848 5.728-6.176 12.8-16.416 20-26.496 5.12-7.232 11.584-8.128 18.368-5.568 6.912 2.4 43.488 20.48 51.008 24.224 7.52 3.776 12.48 5.568 14.304 8.736 1.792 3.168 1.792 18.048-4.384 35.52z" fill="#FAFAFA"/></svg>
+</a>
+</body>
+
+</html>
